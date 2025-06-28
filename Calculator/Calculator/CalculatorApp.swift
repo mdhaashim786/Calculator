b4305b60a6f0780b27a5022de576e39232dba4a0
mhaashim <mdhaashim786@gmail.com>
2025-06-28 18:20:12 +0530

    Create a new project with name Calculator

---
+++ Calculator/Calculator/CalculatorApp.swift
@@ -0,0 +1,17 @@
+//
+//  CalculatorApp.swift
+//  Calculator
+//
+//  Created by mhaashim on 28/06/25.
+//
+
+import SwiftUI
+
+@main
+struct CalculatorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
