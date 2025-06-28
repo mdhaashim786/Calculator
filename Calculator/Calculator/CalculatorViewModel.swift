46381cfeafb2181ee16194eb07f82910048330d8
mhaashim <mdhaashim786@gmail.com>
2025-06-28 20:04:38 +0530

    Implement basic addition function with allowed delimiters

--- Calculator/Calculator/CalculatorViewModel.swift
+++ Calculator/Calculator/CalculatorViewModel.swift
@@ -6,6 +6,7 @@
 //
 
 import SwiftUI
+import Foundation
 
 protocol CalculatorViewModelProtocol: ObservableObject {
     func add(_ numbers: String) -> Int
@@ -14,8 +15,26 @@ protocol CalculatorViewModelProtocol: ObservableObject {
 class CalculatorViewModel: CalculatorViewModelProtocol {
     
     func add(_ numbers: String) -> Int {
-        print(numbers)
-        return Int(numbers) ?? 0
+        
+        // Handle empty string
+        guard !numbers.isEmpty else { return 0 }
+        
+        // To recognise the new line include "\\n"
+        let allowedDelimiters = CharacterSet(charactersIn: ",\\n")
+        let numbersToProcess = numbers
+        
+        let numberStrings = numbersToProcess.components(separatedBy: allowedDelimiters)
+        
+        // Convert to integers
+        let numbersArray = numberStrings.compactMap { Int($0) }
+        
+        var sumOfNumbers = 0
+        
+        for num in numbersArray {
+            sumOfNumbers += num
+        }
+        
+        return sumOfNumbers
     }
     
 }
